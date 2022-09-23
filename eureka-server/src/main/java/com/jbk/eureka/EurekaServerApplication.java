539b87dbf7e3914f1546535de9e0eca79e40b73d
RupaliKshirsagar <rupalimk22@gmail.com>
2022-09-23 21:40:31 +0530

    main-eureka-server

---
+++ eureka-server/src/main/java/com/jbk/eureka/EurekaServerApplication.java
@@ -0,0 +1,19 @@
+package com.jbk.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+}
+
+
+// Eureka Server===> just monitoring which service is in upState or DownState...i.e running or stop
+//server poet:8761
